cc349593dbe040b414d5870a550b813f2d744f9a
Danielon123456789 <h_gdaniel@hotmail.com>
2026-07-21 14:52:42 -0600

    feat: add subscriptions table migration

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,6 @@
 from logging.config import fileConfig
 from app.core.config import settings
-from app.core.database import Base 
-from app.models.user import User
+from app.core.database import Base
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -11,7 +10,7 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url",str(settings.DATABASE_URL))
+config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -68,9 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
